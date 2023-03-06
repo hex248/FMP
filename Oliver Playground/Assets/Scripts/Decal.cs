277fd3357ba0536e67d0eaa351c8bc91ffe85a8a
ob <oliverbryan248@gmail.com>
2023-03-06 10:41:12 +0000

    Imported wood hut with lighting

--- Oliver Playground/Assets/Scripts/Decal.cs
+++ Oliver Playground/Assets/Scripts/Decal.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Decal : MonoBehaviour
 {
+    public bool fade;
     public float initialAlpha;
     public float timeUntilFade;
     public float fadeTime;
+    public Texture decalTexture;
     float timeElapsed = 0.0f;
     Renderer renderer;
 
@@ -14,16 +16,20 @@ public class Decal : MonoBehaviour
     {
         renderer = GetComponent<Renderer>();
         renderer.material.SetFloat("_Alpha", initialAlpha);
+        renderer.material.mainTexture = decalTexture;
     }
 
     private void LateUpdate()
     {
-        timeElapsed += Time.deltaTime;
-        if (timeUntilFade <= timeElapsed)
+        if (fade)
         {
-            float alpha = initialAlpha - (initialAlpha * ((timeElapsed - timeUntilFade) / fadeTime));
-            renderer.material.SetFloat("_Alpha", alpha);
-            if (alpha <= 0) Destroy(gameObject); // if decal is invisible, destroy this game object
+            timeElapsed += Time.deltaTime;
+            if (timeUntilFade <= timeElapsed)
+            {
+                float alpha = initialAlpha - (initialAlpha * ((timeElapsed - timeUntilFade) / fadeTime));
+                renderer.material.SetFloat("_Alpha", alpha);
+                if (alpha <= 0) Destroy(gameObject); // if decal is invisible, destroy this game object
+            }
         }
     }
 }
