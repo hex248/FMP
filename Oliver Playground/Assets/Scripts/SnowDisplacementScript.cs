edd53340292c1008c72562ec5b931aa42e5fe4f6
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-03-07 17:56:44 +0000

    Scaling snow works now :)

--- Oliver Playground/Assets/Scripts/SnowDisplacementScript.cs
+++ Oliver Playground/Assets/Scripts/SnowDisplacementScript.cs
@@ -22,11 +22,12 @@ public class SnowDisplacementScript : MonoBehaviour
     {
         for (int i = 0; i < vertices.Length; i++)
         {
-            if (other.bounds.max.x >= (vertices[i] + transform.position).x && other.bounds.min.x <= (vertices[i] + transform.position).x && other.bounds.max.z >= (vertices[i] + transform.position).z && other.bounds.min.z <= (vertices[i] + transform.position).z)
+            if (other.bounds.max.x >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.min.x <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.max.z >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z && other.bounds.min.z <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z)
             {
                 if (vertices[i].y > -2f && vertices[i].y < 2f)
                 {
                     vertices[i] += Vector3.down * Time.deltaTime * 10f;
+                    mesh.RecalculateNormals();
                 }
             }
         }
