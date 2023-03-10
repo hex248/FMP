9283ee15b71ae5139d06672914da57c6fc7c67fd
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-03-10 18:20:20 +0000

    Snow working :) would be better if it was run on the gpu

--- Oliver Playground/Assets/Scripts/SnowDisplacementScript.cs
+++ Oliver Playground/Assets/Scripts/SnowDisplacementScript.cs
@@ -8,10 +8,12 @@ public class SnowDisplacementScript : MonoBehaviour
     MeshFilter meshFilter;
     List<Vector3> vertices = new List<Vector3>();
     List<Color> colors = new List<Color>();
+    List<int> collidedVertsIndexes = new List<int>();
 
     SnowManager snowManager;
 
     bool highPoly = false;
+    Vector3[] originalVertices;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,58 +21,69 @@ public class SnowDisplacementScript : MonoBehaviour
         meshFilter = GetComponent<MeshFilter>();
         meshFilter.mesh.GetVertices(vertices);
         meshFilter.mesh.GetColors(colors);
+        originalVertices = snowManager.highPolyMesh.vertices;
     }
-
-    int index = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if(index < vertices.Count - 1)
+        if (highPoly)
         {
-            index++;
-        }
-        else
-        {
-            index = 0;
-        }
-        if (vertices[index].y > -0.5f && vertices[index].y < 0.5f)
-        {
-            vertices[index] += Vector3.up * Time.deltaTime * 20f * Mathf.PerlinNoise(vertices[index].x, vertices[index].z);
+            if (collidedVertsIndexes.Count > 0)
+            {
+                for (int i = 0; i < collidedVertsIndexes.Count; i++)
+                {
+                    int j = collidedVertsIndexes[i];
+                    if (vertices[j].y < originalVertices[j].y)
+                    {
+                        vertices[j] += Vector3.up * Time.deltaTime * 0.5f;
+                        colors[j] = new Color(meshFilter.mesh.normals[j].x, meshFilter.mesh.normals[j].y, meshFilter.mesh.normals[j].z);
+                    }
+                    else
+                    {
+                        collidedVertsIndexes.Remove(j);
+                    }
+                }
+            }
+            else
+            {
+                SwitchToLowPoly();
+            }
         }
     }
-    private void OnTriggerStay(Collider other)
+
+    private void LateUpdate()
+    {
+        meshFilter.mesh.SetVertices(vertices);
+        meshFilter.mesh.SetColors(colors);
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (!highPoly)
         {
             SwitchToHighPoly();
         }
-        else
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        for (int i = 0; i < vertices.Count; i++)
         {
-            for (int i = 0; i < vertices.Count - 1; i++)
+            if (other.bounds.max.x >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.min.x <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.max.z >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z && other.bounds.min.z <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z)
             {
-                if (other.bounds.max.x >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.min.x <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.max.z >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z && other.bounds.min.z <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z)
+                if (vertices[i].y > -0.5f && vertices[i].y < 0.5f)
                 {
-                    if (vertices[i].y > -0.5f && vertices[i].y < 0.5f)
+                    vertices[i] += Vector3.down * Time.deltaTime * 10.0f;
+                    colors[i] = new Color(meshFilter.mesh.normals[i].x, meshFilter.mesh.normals[i].y, meshFilter.mesh.normals[i].z);
+                    if (!collidedVertsIndexes.Contains(i) || collidedVertsIndexes.Count == 0)
                     {
-                        colors[i] = new Color(meshFilter.mesh.normals[i].x, meshFilter.mesh.normals[i].y, meshFilter.mesh.normals[i].z);
-                        vertices[i] += Vector3.down * Time.deltaTime * 40f;
+                        collidedVertsIndexes.Add(i);
+                        break;
                     }
                 }
-                /*if (other.bounds.max.x >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.min.x <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).x && other.bounds.max.z >= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z && other.bounds.min.z <= (Vector3.Scale(vertices[i], transform.lossyScale) + transform.position).z)
-                {
-                    if (vertices[i].y > -2f && vertices[i].y < 2f)
-                    {
-                        vertices[i] += Vector3.down * Time.deltaTime * 10f;
-                        colors[i] = new Color(meshFilter.mesh.normals[i].x, meshFilter.mesh.normals[i].y, meshFilter.mesh.normals[i].z);
-                        highPoly = true;
-                    }
-                }*/
             }
-
-            meshFilter.mesh.SetVertices(vertices);
-            meshFilter.mesh.SetColors(colors);
-            meshFilter.mesh.RecalculateNormals();
         }
     }
 
@@ -81,5 +94,13 @@ public class SnowDisplacementScript : MonoBehaviour
         meshFilter.mesh.GetVertices(vertices);
         meshFilter.mesh.GetColors(colors);
         highPoly = true;
+    }
+    void SwitchToLowPoly()
+    {
+        Debug.Log("Switch to low poly");
+        meshFilter.mesh = snowManager.lowPolyMesh;
+        meshFilter.mesh.GetVertices(vertices);
+        meshFilter.mesh.GetColors(colors);
+        highPoly = false;
     }
 }
