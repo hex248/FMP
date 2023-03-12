cad05ad8e91b424e2ecd6880ece99d03db329e34
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-03-12 20:01:35 +0000

    Updated Concept art

--- Oliver Playground/Assets/Scripts/ClothSimulationScript.cs
+++ Oliver Playground/Assets/Scripts/ClothSimulationScript.cs
@@ -17,12 +17,23 @@ class Particle
     public void Simulate()
     {
         this.force = Physics.gravity * this.mass;
-        if (Physics.Raycast(this.location, velocity.normalized, out RaycastHit hit) && hit.distance <= 0.5f)
+        Collider[] others = Physics.OverlapSphere(this.location, 0.5f, ~0, QueryTriggerInteraction.Ignore);
+        if(others.Length > 0)
         {
-            float reactionForce = -(this.velocity.y / Time.fixedDeltaTime) * this.mass;
-            this.force += reactionForce * hit.normal;
+            foreach(Collider other in others)
+            {
+                if (Physics.Raycast(this.location, other.ClosestPoint(this.location) - this.location, out RaycastHit hit))
+                {
+                    Vector3 force = this.force + ((this.velocity / Time.fixedDeltaTime) * this.mass);
+                    Vector3 reactionForce = this.mass * force * Mathf.Cos(Vector3.Angle(Vector3.down, -hit.normal));
+                    Vector3 frictionForce = this.mass * force * Mathf.Sin(Vector3.Angle(Vector3.down, -hit.normal));
+                    //this.force -= reactionXForce * Vector3.right * Mathf.Sin(Vector3.Angle(Vector3.up, hit.normal));
+                    this.force -= reactionForce;
+                    //this.force -= frictionForce * 0.4f;
+                }
+            }
         }
-        this.velocity += (this.force * Time.fixedDeltaTime) / this.mass;
+        this.velocity += (this.force / this.mass) * Time.fixedDeltaTime;
         this.location += this.velocity * Time.fixedDeltaTime;
     }
 };
@@ -34,19 +45,31 @@ public class ClothSimulationScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        particles = new Particle[1];
-        particles[0] = new Particle(1.0f, Vector3.up * 10.0f);
+        particles = new Particle[40];
+        for(int i = 0; i < particles.Length - 1; i++)
+        {
+            particles[i] = new Particle(1.0f, Vector3.up * 10.0f + Vector3.right * i * 0.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = particles[0].location;
     }
 
     private void FixedUpdate()
     {
-        particles[0].Simulate();
-        transform.position = particles[0].location;
+        for (int i = 0; i < particles.Length - 1; i++)
+        {
+            particles[i].Simulate();
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        for(int i = 1; i < particles.Length - 1; i++)
+        {
+            Gizmos.DrawLine(particles[i - 1].location, particles[i].location);
+        }
     }
 }
