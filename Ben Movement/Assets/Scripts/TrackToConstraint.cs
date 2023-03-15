3165f5134ed3b1637dfee1e27e4ce3d20b32c854
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-03-15 20:43:41 +0000

    Updated Idle animation and Walk animation

---
+++ Ben Movement/Assets/Scripts/TrackToConstraint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class TrackToConstraint : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
