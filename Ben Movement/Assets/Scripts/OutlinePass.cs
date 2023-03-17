8827ee136ff961fc19681d85607945626ff8e2fe
Binjabin <94044545+Binjabin@users.noreply.github.com>
2023-03-17 11:47:44 +0000

    More research into outlines. Moving to shader-graph.
    Found a good video of a URP implementation of sroystan's outlines
    
    https://www.youtube.com/watch?v=LMqio9NsqmM

--- Ben Movement/Assets/Scripts/OutlinePass.cs
+++ Ben Movement/Assets/Scripts/OutlinePass.cs
@@ -5,14 +5,11 @@ using UnityEngine.Rendering.Universal;
 
 public class OutlinePass : ScriptableRenderPass
 {
-    // The profiler tag that will show up in the frame debugger.
-    const string ProfilerTag = "Outline Pass";
-
-    // We will store our pass settings in this variable.
     OutlineFeature.OutlineSettings passSettings;
 
     RenderTargetIdentifier colorBuffer, temporaryBuffer;
     int temporaryBufferID = Shader.PropertyToID("_TemporaryBuffer");
+    const string ProfilerTag = "Outline Pass";
 
     Material material;
 
@@ -26,7 +23,7 @@ public class OutlinePass : ScriptableRenderPass
 
         // We create a material that will be used during our pass. You can do it like this using the 'CreateEngineMaterial' method, giving it
         // a shader path as an input or you can use a 'public Material material;' field in your pass settings and access it here through 'passSettings.material'.
-        if (material == null) material = passSettings.shader;
+        if (material == null) material = CoreUtils.CreateEngineMaterial("Hidden/Binjabin/Outline");
 
         // Set any material properties based on our pass settings. 
         material.SetFloat("_Scale", passSettings.scale);
@@ -37,7 +34,6 @@ public class OutlinePass : ScriptableRenderPass
         material.SetFloat("_DepthNormalThresholdScale", passSettings.depthNormalThresholdScale);
         material.SetColor("_Color", passSettings.color);
         
-        //Matrix4x4 clipToView = GL.GetGPUProjectionMatrix(passSettings.camera.projectionMatrix, true).inverse;
         Matrix4x4 clipToView = Matrix4x4.zero;
         material.SetMatrix("_ClipToView", clipToView);
     }
@@ -51,15 +47,15 @@ public class OutlinePass : ScriptableRenderPass
         // Grab the camera target descriptor. We will use this when creating a temporary render texture.
         RenderTextureDescriptor descriptor = renderingData.cameraData.cameraTargetDescriptor;
 
-        // Set the number of depth bits we need for our temporary render texture.
-        descriptor.depthBufferBits = 0;
-
-        // Enable these if your pass requires access to the CameraDepthTexture or the CameraNormalsTexture.
-        // ConfigureInput(ScriptableRenderPassInput.Depth);
-        // ConfigureInput(ScriptableRenderPassInput.Normal);
-
         // Grab the color buffer from the renderer camera color target.
         colorBuffer = renderingData.cameraData.renderer.cameraColorTarget;
+
+        // Enable these if your pass requires access to the CameraDepthTexture or the CameraNormalsTexture.
+        //ConfigureInput(ScriptableRenderPassInput.Depth);
+        //ConfigureInput(ScriptableRenderPassInput.Normal);
+
+        Matrix4x4 clipToView = GL.GetGPUProjectionMatrix(renderingData.cameraData.camera.projectionMatrix, true).inverse;
+        material.SetMatrix("_ClipToView", clipToView);
 
         // Create a temporary render texture using the descriptor from above.
         cmd.GetTemporaryRT(temporaryBufferID, descriptor, FilterMode.Point);
@@ -71,10 +67,13 @@ public class OutlinePass : ScriptableRenderPass
     {
 
         // Grab a command buffer. We put the actual execution of the pass inside of a profiling scope.
+
+
+
         CommandBuffer cmd = CommandBufferPool.Get();
         cmd.Clear();
-        Blit(cmd, colorBuffer, temporaryBuffer, material, 0); // shader pass 0
-
+        Blit(cmd, colorBuffer, temporaryBuffer, material); // shader pass 0
+        Blit(cmd, temporaryBuffer, colorBuffer);
 
         // Execute the command buffer and release it.
         context.ExecuteCommandBuffer(cmd);
