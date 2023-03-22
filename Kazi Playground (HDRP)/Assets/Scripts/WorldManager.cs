f2beffbe05cd088ca23e03735e0d4afc6c875559
hex248 <oliverbryan248@gmail.com>
2023-03-22 17:47:38 +0000

    Removed unnecessary projects

--- Kazi Playground (HDRP)/Assets/Scripts/WorldManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WorldManager : MonoBehaviour
-{
-    public Transform wind;
-    public float fogDensity;
-}
