4cf29f2b00dc9a1e25581b35259f52aff81fd802
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-03-28 22:21:24 +0100

    More outline and also oblique camera projection

--- Ben Movement/Assets/Scripts/CameraFollow.cs
+++ Ben Movement/Assets/Scripts/CameraFollow.cs
@@ -8,9 +8,18 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform playerPoint;
     [SerializeField] Camera cam;
     [SerializeField] float smoothSpeed;
+    public float hori, verti;
     Transform currentFocusPoint;
     Rigidbody rb;
     private Vector3 velocity = Vector3.zero;
+
+    void SetObliqueness(float horizObl, float vertObl)
+    {
+        Matrix4x4 mat = Camera.main.projectionMatrix;
+        mat[0, 2] = horizObl;
+        mat[1, 2] = vertObl;
+        Camera.main.projectionMatrix = mat;
+    }
 
     void Start()
     {
@@ -24,6 +33,7 @@ public class CameraFollow : MonoBehaviour
         {
             transform.position = Vector3.SmoothDamp(transform.position, currentFocusPoint.position, ref velocity, smoothSpeed);
         }
+        SetObliqueness(hori, verti);
     }
 
     public void Focus(Transform focusTarget)
