c3b7601862d58d9ce9cd7240ffea3a6ab92b83a4
Binjabin <94044545+Binjabin@users.noreply.github.com>
2023-03-30 12:45:37 +0100

    Added wolf attack with animation and possibility for damage later.
    Need better drawback animation, and recovery

--- Ben Movement/Assets/Scripts/WolfController.cs
+++ Ben Movement/Assets/Scripts/WolfController.cs
@@ -6,15 +6,31 @@ public class WolfController : MonoBehaviour
 {
     Rigidbody rb;
     float timeSinceStart;
-    
-    
-    [Header("Controller")]
+
+
+    [Header("Movement")]
     [SerializeField] float moveSpeed;
     [SerializeField] float moveRotationSpeed;
     [SerializeField] float aimRotationSpeed;
-    [SerializeField] float attackDistance;
+    [SerializeField] float inRangeMoveSpeed;
+    Quaternion rotationalDirection;
 
+    [Header("Attack")]
+    [SerializeField] float attackTime;
+    [SerializeField] float attackMoveDistance;
+    [SerializeField] float attackMoveTime;
+    [SerializeField] float damageTime;
+    [SerializeField] float attackCooldown;
+    bool doneAttackHit;
+    float timeSinceAttackEnd;
+    float timeSinceLastAttacking;
+    Vector3 currentAttackDirection;
     bool isAttacking;
+    [SerializeField] Vector3 hitboxOffset;
+    [SerializeField] Vector3 hitboxSize;
+    [SerializeField] LayerMask attackLayerMask;
+    [SerializeField] float attackForce;
+    [SerializeField] float attackDamage;
 
     [Header("Visuals")]
     [SerializeField] GameObject wolfVisuals;
@@ -36,10 +52,11 @@ public class WolfController : MonoBehaviour
         bed = FindObjectOfType<Bed>();
         wolfAnimationScript = GetComponentInChildren<WolfAnimationScript>();
         currentTarget = bed.gameObject;
+        timeSinceLastAttacking = 0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 offsetToTarget = currentTarget.transform.position - transform.position;
         Vector3 directionToTarget = offsetToTarget.normalized;
@@ -47,38 +64,57 @@ public class WolfController : MonoBehaviour
         bool targetIsInDirectView = false;
 
         RaycastHit hit;
-        Physics.Raycast(transform.position, directionToTarget, out hit, Mathf.Infinity, raycastLayer);
-        if(hit.collider != null)
+        float minDistanceToTargetCollider = Mathf.Infinity;
+        Vector3 raycastStart = transform.position + new Vector3(0f, 0.5f, 0f);
+        Physics.Raycast(raycastStart, directionToTarget, out hit, Mathf.Infinity, raycastLayer);
+        if (hit.collider != null)
         {
             targetIsInDirectView = (hit.collider.gameObject == currentTarget);
+            minDistanceToTargetCollider = (hit.point - transform.position).magnitude;
         }
-        float minDistanceToTargetCollider = (hit.point - transform.position).magnitude;
-        Debug.Log("distance to collider is " + minDistanceToTargetCollider);
-        //decide behaviour
-        if (minDistanceToTargetCollider <= maxAttackRange)
-        {
-            //in range
 
-            if(Vector3.Dot(transform.forward, directionToTarget) >= 0.5f)
+
+        if (!isMovementLocked())
+        {
+            if (minDistanceToTargetCollider <= maxAttackRange)
             {
-                //facing right direction
-                if(!isAttacking)
-                    Attack();
+                //in range
+
+                if (Vector3.Dot(transform.forward, directionToTarget) >= 0.9f && timeSinceLastAttacking >= attackCooldown)
+                {
+                    //facing right direction
+                    StartAttack();
+                }
+                else
+                {
+                    //need to aim
+                    AimTowardsTarget();
+                    MoveForward(inRangeMoveSpeed);
+                }
             }
             else
             {
-                //need to aim
-                AimTowardsTarget();
+                RotateTowardsTarget();
+                MoveForward(moveSpeed);
+            }
+        }
+
+
+
+        if (isAttacking)
+        {
+            timeSinceLastAttacking = 0f;
+            DoMeleeAttackMove();
+            if (timeSinceAttackEnd >= damageTime && doneAttackHit == false)
+            {
+                DoMeleeAttackHit();
             }
         }
         else
         {
-            RotateTowardsTarget();
-            MoveForward(moveSpeed);
+            timeSinceLastAttacking += Time.deltaTime;
         }
 
-        
-        
 
         if (targetPlayer)
         {
@@ -90,27 +126,96 @@ public class WolfController : MonoBehaviour
 
     void MoveForward(float speed)
     {
-        Vector3 desiredVelocity = wolfVisuals.transform.forward * moveSpeed;
+        Vector3 desiredVelocity = wolfVisuals.transform.forward * speed;
         rb.velocity = desiredVelocity;
     }
 
     void RotateTowardsTarget()
     {
         Quaternion targetRotation = Quaternion.LookRotation(currentTarget.transform.position - transform.position, Vector3.up);
-        transform.rotation = Quaternion.Lerp(wolfVisuals.transform.rotation, targetRotation, moveRotationSpeed * Time.deltaTime);
+
+        rotationalDirection = Quaternion.Lerp(wolfVisuals.transform.rotation, targetRotation, moveRotationSpeed * Time.deltaTime);
+        transform.rotation = rotationalDirection;
         //smoke.SetVector3("EmissionDirection", head.forward * -1);
     }
 
     void AimTowardsTarget()
     {
         Quaternion targetRotation = Quaternion.LookRotation(currentTarget.transform.position - transform.position, Vector3.up);
+
         transform.rotation = Quaternion.Lerp(wolfVisuals.transform.rotation, targetRotation, aimRotationSpeed * Time.deltaTime);
         //smoke.SetVector3("EmissionDirection", head.forward * -1);
     }
 
-    void Attack()
+    bool isMovementLocked()
+    {
+        return isAttacking;
+    }
+
+    void StartAttack()
     {
         isAttacking = true;
         wolfAnimationScript.StartAttackAnimation();
+
+        //...
+        timeSinceAttackEnd = 0f;
+        doneAttackHit = false;
+        currentAttackDirection = transform.forward;
+    }
+
+    void DoMeleeAttackMove()
+    {
+        if (timeSinceAttackEnd >= attackTime)
+        {
+            isAttacking = false;
+
+            //time you were last  attacking
+
+        }
+        else if (timeSinceAttackEnd >= attackMoveTime)
+        {
+            //finished attack movement
+        }
+        else
+        {
+            //move player
+            float attackSpeed = attackMoveDistance / attackMoveTime;
+            MoveForward(attackSpeed);
+        }
+        timeSinceAttackEnd += Time.deltaTime;
+    }
+
+    void DoMeleeAttackHit()
+    {
+        doneAttackHit = true;
+        Vector3 offset = Quaternion.AngleAxis(rotationalDirection.eulerAngles.y, Vector3.up) * hitboxOffset;
+        Collider[] hitColliders = Physics.OverlapBox(transform.position + offset, (hitboxSize / 2f), rotationalDirection, attackLayerMask);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Rigidbody hitRb = hitColliders[i].gameObject.GetComponent<Rigidbody>();
+            if (hitRb != null)
+            {
+                Debug.Log("Apply force to " + hitRb.gameObject);
+                hitRb.AddForce(currentAttackDirection * attackForce);
+            }
+            EnemyHealth enemy = hitColliders[i].gameObject.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+
+        Gizmos.matrix = transform.localToWorldMatrix;
+            //Vector3 offset = Quaternion.AngleAxis(rotationalDirection.eulerAngles.y, Vector3.up) * meleeCombo[currentMeleeComboStage].hitboxOffset;
+        Gizmos.DrawWireCube(hitboxOffset, hitboxSize);
+
+
+
     }
 }
