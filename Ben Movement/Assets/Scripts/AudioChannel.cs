565fa42f5003f604ea3d9699875a11d3e0ff6c35
hex248 <oliverbryan248@gmail.com>
2023-04-19 19:04:06 +0100

    Mixer system complete

--- Ben Movement/Assets/Scripts/AudioChannel.cs
+++ Ben Movement/Assets/Scripts/AudioChannel.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 public class AudioChannel : MonoBehaviour
 {
     public int channelNumber;
-    public float volume;
     public ChannelType type;
     public AudioSource source;
+    public AudioClip mainClip;
 
-    public void Play(AudioClip clip)
+    AudioManager audioManager;
+    bool fading = false;
+    float fadeStart = 0.0f;
+    float fadeEnd = 0.0f;
+    float fadeSpeed = 0.0f;
+
+    public void SetClip(AudioClip clip)
+    {
+        mainClip = clip;
+        source.clip = mainClip;
+    }
+
+    public void Play()
+    {
+        source.Play();
+    }
+
+    public void PlayOnce(AudioClip clip)
     {
         source.PlayOneShot(clip);
+    }
+
+    public void Fade(float startVol, float endVol, float speed)
+    {
+        fadeStart = startVol;
+        fadeEnd = endVol;
+        fadeSpeed = speed;
+        fading = true;
+    }
+
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    private void Update()
+    {
+        if (fading)
+        {
+            float step = Mathf.Abs(fadeStart - fadeEnd) / fadeSpeed; // amount to change per second
+            float newVolume = Mathf.Clamp(Mathf.Lerp(source.volume, fadeEnd, step * Time.deltaTime), fadeStart, fadeEnd); // lerp between current volume and target volume
+            audioManager.mixer.Find(e => e.sourceName == gameObject.name).volume = newVolume; // set new volume in mixer
+            if (newVolume == fadeEnd) fading = false;
+        }
     }
 }
--- Ben Movement/Assets/Scripts/AudioManager.cs
+++ Ben Movement/Assets/Scripts/AudioManager.cs
@@ -9,29 +9,99 @@ public enum ChannelType
     SFX
 }
 
+[System.Serializable]
+public class AudioChannelSettings
+{
+    public string sourceName;
+    [Range(0, 1)]
+    public float volume = 1.0f;
+    [Range(-3, 3)]
+    public float pitch = 1.0f;
+    [Range(0, 255)]
+    public int priority = 128;
+    public bool mute = false;
+    public bool playOnAwake = false;
+    public bool loop = false;
+}
+
+[System.Serializable]
+public class AudioAsset
+{
+    public string name;
+    public ChannelType type;
+    public AudioClip clip;
+}
 
 [ExecuteAlways]
 public class AudioManager : MonoBehaviour
 {
+    [Header("Settings")]
+    [Range(0, 10)]
+    public float musicTransitionSpeed = 1.0f;
+
+    [Header("Attributes")]
+    public List<AudioChannelSettings> mixer;
+    public List<AudioAsset> assets;
     public List<AudioChannel> audioChannels;
 
     private void Update()
     {
-        if (Application.isEditor)
+        if (!Application.isPlaying)
         {
-            List<AudioChannel> newAudioChannels = new List<AudioChannel>();
-            
-            foreach (Transform child in transform)
+            UpdateChannelsAndMixer();
+        }
+    }
+
+    void UpdateChannelsAndMixer()
+    {
+        List<AudioChannel> newAudioChannels = new List<AudioChannel>();
+        foreach (Transform child in transform)
+        {
+            newAudioChannels.Add(child.GetComponent<AudioChannel>());
+        }
+        audioChannels = newAudioChannels;
+
+        /*
+         * if the audio channels have changed
+         * recreated the settings list
+        */
+        if (mixer.Count != audioChannels.Count)
+        {
+            List<AudioChannelSettings> newMixer = new List<AudioChannelSettings>();
+            for (int i = 0; i < audioChannels.Count; i++)
             {
-                newAudioChannels.Add(child.GetComponent<AudioChannel>());
+                var settings = new AudioChannelSettings();
+                settings.sourceName = audioChannels[i].name;
+                settings.volume = audioChannels[i].source.volume;
+                settings.pitch = audioChannels[i].source.pitch;
+                settings.priority = audioChannels[i].source.priority;
+                settings.mute = audioChannels[i].source.mute;
+                settings.playOnAwake = audioChannels[i].source.playOnAwake;
+                settings.loop = audioChannels[i].source.loop;
+
+                newMixer.Add(settings);
             }
 
-            audioChannels = newAudioChannels;
+            mixer = newMixer;
+        }
+
+        for (int i = 0; i < audioChannels.Count; i++)
+        {
+            var source = audioChannels[i].source;
+            var settings = mixer[i];
+
+            //apply settings
+            source.volume = settings.volume;
+            source.pitch = settings.pitch;
+            source.priority = settings.priority;
+            source.mute = settings.mute;
+            source.playOnAwake = settings.playOnAwake;
+            source.loop = settings.loop;
         }
     }
 
     public void PlayInChannel(AudioClip clip, int channelNumber)
     {
-        audioChannels[channelNumber - 1].Play(clip);
+        audioChannels[channelNumber - 1].PlayOnce(clip);
     }
 }
