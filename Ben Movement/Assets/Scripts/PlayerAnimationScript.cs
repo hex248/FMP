51c66e93884903c91c4a0c708ca63c0f095db4bc
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-04-20 17:56:33 +0100

    Animations HFR

--- Ben Movement/Assets/Scripts/PlayerAnimationScript.cs
+++ Ben Movement/Assets/Scripts/PlayerAnimationScript.cs
@@ -28,7 +28,7 @@ public class PlayerAnimationScript : MonoBehaviour
     private void Update()
     {
         anim.SetBool("isMoving", isMoving);
-        anim.SetFloat("movementSpeed", rb.velocity.magnitude);
+        anim.SetFloat("movementSpeed", rb.velocity.magnitude * movementSpeedAnimation);
     }
 
     public void StartRangedAttackAnimation()
