54d42a765763df77906a1e224184968ae97ae535
Oliver Bryan <oliverbryan248@gmail.com>
2023-04-26 11:47:47 +0100

    Navmesh in main scene

--- Ben Movement/Assets/Scripts/HealingTrap.cs
+++ Ben Movement/Assets/Scripts/HealingTrap.cs
@@ -15,7 +15,7 @@ public class HealingTrap : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             // play healing sound in SFX Channel 1
-            AM.PlayInChannel("healing-trap_heal", ChannelType.SFX, 1); //! IMPORTANT - CHANGE THIS  SOUND ONCE HEALING SOUND HAS BEEN IMPORTED
+            AM.PlayInChannel("healing-trap_heal", ChannelType.SFX, 1);
         }
     }
 }
