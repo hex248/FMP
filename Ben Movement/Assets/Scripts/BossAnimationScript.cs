6c9b696c6741bd90ed5efffdf68ca0f8a6bbcdbf
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2023-04-28 11:50:50 +0100

    Boss glides towards player

---
+++ Ben Movement/Assets/Scripts/BossAnimationScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimationScript : MonoBehaviour
+{
+    public float movementSpeedAnimation = 1.0f;
+    private Animator anim;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartMoving()
+    {
+        anim.SetBool("isMoving", true);
+    }
+
+    public void EndMoving()
+    {
+        anim.SetBool("isMoving", false);
+    }
+}
---
+++ Ben Movement/Assets/Scripts/BossController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float moveRotationSpeed = 1.0f;
+    [SerializeField] float range = 1.0f;
+    [SerializeField] Transform target;
+    private BossAnimationScript animationScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationScript = GetComponentInChildren<BossAnimationScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Scale(target.position - transform.position, Vector3.one - Vector3.up).magnitude > range)
+        {
+            MovingState();
+            animationScript.StartMoving();
+        }
+        else
+        {
+            animationScript.EndMoving();
+        }
+    }
+
+    void MovingState()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        float Y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0.0f, Mathf.LerpAngle(transform.eulerAngles.y, Y - 90.0f, Time.deltaTime * moveRotationSpeed), 0.0f);
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+    }
+}
