318db92de72f696294b4fb6fcc6bb350f9ef84df
Oliver Bryan <oliverbryan248@gmail.com>
2023-05-03 19:09:42 +0100

    Flower particles

---
+++ Ben Movement/Assets/VFX/Player/Green/FlowerParticles.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteAlways]
+public class FlowerParticles : MonoBehaviour
+{
+    VisualEffect VFX;
+
+    [Header("Settings")]
+    [SerializeField] Vector3 spawnOffset;
+    [SerializeField] Texture2D[] flowers;
+    public Texture texture;
+    public int index = 0;
+
+    private void Start()
+    {
+        VFX = GetComponent<VisualEffect>();
+    }
+    
+    void Update()
+    {
+        VFX.SetVector3("spawnPosition", transform.position + spawnOffset);
+        VFX.SetVector3("direction", transform.right);
+        texture = flowers[index];
+        index++;
+        if (index >= flowers.Length) index = 0;
+    }
+}
