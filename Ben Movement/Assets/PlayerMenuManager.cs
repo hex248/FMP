971a0013533dd57c1cb09badd4a69bca911946c7
Ben Farrant Smith <94044545+Binjabin@users.noreply.github.com>
2023-05-05 15:56:51 +0100

    Working on start menu joining.

--- Ben Movement/Assets/PlayerMenuManager.cs
+++ Ben Movement/Assets/PlayerMenuManager.cs
@@ -7,13 +7,79 @@ public class PlayerMenuManager : MonoBehaviour
     List<MenuPlayer> players = new List<MenuPlayer>();
 
     [SerializeField] List<GameObject> playerJoinUIVisuals;
+    [SerializeField] List<GameObject> promptsUIVisuals;
+
+    [SerializeField] AnimationCurve playerJoinAnimationSpeed;
 
 
     public void PlayerJoined(MenuPlayer player)
     {
+        StartCoroutine(SlideInVisual(player.playerIndex));
+        Debug.Log("Start co-routine");
+    }
 
+    IEnumerator SlideInVisual(int playerIndex)
+    {
+        playerJoinUIVisuals[playerIndex - 1].SetActive(true);
+        GameObject visual = playerJoinUIVisuals[playerIndex - 1];
+        RectTransform rect = visual.GetComponent<RectTransform>();
+
+        rect.localPosition = new Vector3(rect.localPosition.x, -450f, rect.localPosition.z);
+
+        float fac = 0f;
+        float smoothFac = 0f;
+
+        while (fac <= 1f)
+        {
+            fac += Time.deltaTime * 2f;
+            smoothFac = playerJoinAnimationSpeed.Evaluate(fac);
+            float y = (450f * smoothFac) - 450f;
+
+            rect.localPosition = new Vector3(0f, y, 0f);
+            yield return null;
+        }
+        rect.localPosition = new Vector3(rect.localPosition.x, 0f, rect.localPosition.z);
+
+        //yield return new WaitForSeconds(1f);
+        StartCoroutine(SlideOutPromptVisual(playerIndex));
+    }
+
+    IEnumerator SlideOutPromptVisual(int playerIndex)
+    {
+        GameObject visual = promptsUIVisuals[playerIndex - 1];
+        RectTransform rect = visual.GetComponent<RectTransform>();
+
+        rect.localPosition = new Vector3(rect.localPosition.x, 10f, rect.localPosition.z);
+
+        float fac = 1f;
+        float smoothFac = 0f;
+
+        while (fac >= 0f)
+        {
+            fac -= Time.deltaTime * 2f;
+            smoothFac = playerJoinAnimationSpeed.Evaluate(fac);
+            float y = (450f * smoothFac) - 440f;
+
+            rect.localPosition = new Vector3(0f, y, 0f);
+            yield return null;
+        }
+        rect.localPosition = new Vector3(rect.localPosition.x, -440f, rect.localPosition.z);
+        promptsUIVisuals[playerIndex - 1].SetActive(false);
+    }
+
+    void ReadyPlayer(int playerIndex)
+    {
+        StartCoroutine(SlideOutPromptVisual(playerIndex));
+    }
+
+    private void Start()
+    {
+        foreach(GameObject playerJoinVisual in playerJoinUIVisuals)
+        {
+            playerJoinVisual.SetActive(false);
+        }
     }
 
 
-    
+
 }
