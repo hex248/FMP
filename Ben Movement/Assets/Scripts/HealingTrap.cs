26ce5d7aa908559d2200de9236e38cffe1687d13
Oliver Bryan <oliverbryan248@gmail.com>
2023-05-06 21:49:36 +0100

    Started unique turrets and made healer and teleporter rise from the ground

--- Ben Movement/Assets/Scripts/HealingTrap.cs
+++ Ben Movement/Assets/Scripts/HealingTrap.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class HealingTrap : MonoBehaviour
 {
     AudioManager AM;
+
+    [Header("Visuals")]
+    public GameObject[] lights;
+    public GameObject visuals;
+    Vector3 towerPos;
     void Start()
     {
         AM = FindObjectOfType<AudioManager>();
+        towerPos = visuals.transform.position;
+        StartCoroutine(SpawnTurret());
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,6 +24,37 @@ public class HealingTrap : MonoBehaviour
             // play healing sound in SFX Channel 1
             AM.PlayInChannel("healing-trap_heal", ChannelType.SFX, 1);
             other.GetComponent<PlayerTrigger>().health.Heal(1);
+        }
+    }
+
+    IEnumerator SpawnTurret()
+    {
+        visuals.transform.position = new Vector3(towerPos.x, -10.0f, towerPos.z);
+        foreach (GameObject light in lights)
+        {
+            light.SetActive(false);
+        }
+
+        float moveSpeed = 5.0f;
+        float distanceToMove = (towerPos - visuals.transform.position).sqrMagnitude;
+
+        for (; ; )
+        {
+            // move towards turretPos
+
+            visuals.transform.position = Vector3.Lerp(visuals.transform.position, towerPos, moveSpeed * Time.deltaTime);
+
+            if (visuals.transform.position == towerPos)
+            {
+                break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        foreach (GameObject light in lights)
+        {
+            light.SetActive(true);
         }
     }
 }
